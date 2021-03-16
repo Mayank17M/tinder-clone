847c77e6bf3f4d5df0357464be96feb13717c457
Mayank17M <mamgain.mayank01@gmail.com>
2021-03-17 01:35:23 +0530

    Make Backend

--- src/TinderCards.js
+++ src/TinderCards.js
@@ -1,15 +1,21 @@
 import { SwipeableDrawer } from "@material-ui/core";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TinderCard from "react-tinder-card";
 import "./TinderCards.css";
+import axios from "./axios";
 
 function TinderCards() {
-    const [people, setPeople] = useState([
-        {
-            name: "Elon Musk",
-            url: ""
+    const [people, setPeople] = useState([]); 
+
+    useEffect(() => {
+        async function fetchData(){
+            const req = await axios.get('/tinder/cards');
+
+            setPeople(req.data);
         }
-    ]); 
+
+        fetchData();
+    }, [])
 
     const swiped = (direction, nameToDelete) => {
         console.log("removing: "+nameToDelete);
